3eb1eab2b78b0da0a0603691a62b5b0f05a051f6
flyhighrishi <rishikeshshukla47@gmail.com>
2021-03-08 13:40:06 +0530

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
-import "bootstrap/dist/css/bootstrap.min.css";    
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";    
+
 
 function App() {
   return (
